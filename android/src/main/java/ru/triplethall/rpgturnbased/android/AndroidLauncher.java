17eecfee76139a4e136c64d9f4536c49e33ee328
Артём Мартынов | Artyom Martynov <dev@triplethall.ru>
2026-03-04 01:46:12 +0300

    Initial commit

---
+++ android/src/main/java/ru/triplethall/rpgturnbased/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package ru.triplethall.rpgturnbased.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.triplethall.rpgturnbased.RPGTurnbased;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new RPGTurnbased(), configuration);
+    }
+}
