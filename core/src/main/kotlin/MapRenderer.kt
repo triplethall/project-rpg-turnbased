a6bf19e7643c09bbddb5881546edb9c1e98f3353
R4ckstarMade <pigaslv@bk.ru>
2026-05-29 08:51:16 +0300

    trap asset
    добавил ассет ловушки.. но по моему он сломал (подкорроктерую в будущем)

--- core/src/main/kotlin/MapRenderer.kt
+++ core/src/main/kotlin/MapRenderer.kt
@@ -27,6 +27,7 @@ class MapRenderer (
     private val dirtTexture: Texture
     private val mtnTexture: Texture
     private val caveTexture: Texture
+    private val trapTriggeredTexture: Texture
     private lateinit var beachCorner: TextureRegion
     private val CORNER_W = 32f
     private val CORNER_H = 32f
@@ -106,6 +107,7 @@ class MapRenderer (
             TextureRegion(Texture("map_layers/grass_spoilers/light_grass$i.png"))
         }
         caveTexture = Texture("caves/cave0.png")
+        trapTriggeredTexture = Texture("trap_triggered.png")
         generateGrassVariations()
     }
 
@@ -341,13 +343,17 @@ class MapRenderer (
                         batch.color = Color.YELLOW
                         font.draw(batch, "!", posX + cellSize/2 - 5f, posY + cellSize - 10f)
                     }
+                    TerrainType.TRAP_TRIGGERED -> {
+                        batch.color = Color(1f, 1f, 1f, 1f).mul(light, light, light, 1f)
+                        batch.draw(trapTriggeredTexture, posX, posY, cellSize, cellSize)
+                    }
                     else -> {
                         val color = when (terrain) {
                             TerrainType.MOUNTAIN -> continue
                             TerrainType.CITY -> continue
                             TerrainType.ENEMY -> Color.RED
-                            TerrainType.TRAP -> Color.GRAY
-                            TerrainType.TRAP_TRIGGERED -> Color.DARK_GRAY
+                            TerrainType.TRAP -> continue
+                            TerrainType.TRAP_TRIGGERED -> continue
                             TerrainType.UPGRADE -> continue
                             TerrainType.OUTPOST -> Color.CORAL
                             TerrainType.FOREST -> continue
